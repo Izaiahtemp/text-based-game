c713532c99364752a8fb5b8aa27c3a04676288fd
IzaiahTempleton <20250673-IzaiahTempleton@users.noreply.replit.com>
2023-05-24 23:40:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+#adventure game
+import time 
+choice_1 = input(" you have been locked up in this prison for 2 years \n i think it's time to escape do you agree type start when you are ready ") 
+if choice_1 == "start" or "Start":
+  print('you start gathering information to help you escape ')
+  time.sleep(1)
+
+choice_2 = input(" while sitting at lunch you see a door left slightly open \n if you want to try and escape now press N if you want to wait and see if a better  opportunity comes about press W ") 
+time.sleep(1)
+if choice_2 == "N" or choice_2 == "n": 
+  print('you make a run for the door when the guard stops looking')
+  time.sleep(1)
+  ( 'you make it to the door as you run in you see two guards running at you from the left you turn to the right there is a door \n you check to see if it is unlocked but that only wated time the gards catch you and add another 10 yeas onto your sentance \n GAME OVER! ') 
+  exit()
+
+if choice_2 == "w" or choice_2 == "W":
+  choice_3 = input('you are doing your daily job in the tool shed you notice there are no guards around they are all distracted by a fight in the yard \n do you want to steal a hammer press S if you dont want to risk it press D ')
+
+if choice_3 == "d" or choice_3 == "D":
+  print (' You leave the hammer behind as you are leaving you notice you are alone in the yard you look around confused but you wont let this opportunity go to waste ')
+  time.sleep(1)
+  print (' you go to the laundry room and steel a pile of sheets and make a rope to climb over the gate \n as you are climbing over the gate the guards must have spotted you because you hear an alarm go off \n  you make it out of the prison you keep running until you hear dogs barking you turn back there are about ten police dogs chacing you you keep running as fast as you can but they catch you and that was the last anybody heard about you \n  GAME OVER  ')
+  exit() 
+
+else: 
+    print ('you steal the hammer and leave trying not to look suspicious')
+    time.sleep(1)
+    print('it has been 2 days since you stole the hammer and you finnaly think you know the perfect time to start hammering at your wall \n every lunch there is always yelling and fighting \n so you decide to stay back at lunch and as soon as the yelling starts you start smashing the wall ')
+  
+time.sleep(1)
+
+choice_4 = input(" you realise how long this is going to take \n press B to keep digging ")
+
+if choice_4 == "b" or "B":
+  print("you keep digging it is taking ages \n but you are almost through the next day you get through the wall and you get in the vent kick through the door and jump onto the roof you take a minor fall on the way down but you run and finally escape \n YOU WIN   ")
+  exit()
+
+
+
+    
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
